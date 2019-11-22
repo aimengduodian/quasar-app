6daf80a9f9363f15d8af16e0a6c0110a7b45f18c
gpj <1549361150@qq.com>
2019-11-22 11:16:11 +0800

    modify pages

--- src/pages/ebook/coach/add.vue
+++ src/pages/ebook/coach/add.vue
@@ -16,31 +16,47 @@
       </template>
     </q-input>
 
-    <q-input v-model="teach.type" type="number" prefix="类别:" suffix="选择类型">
+    <q-select v-model="type" :options="options" prefix="类别:" suffix="选择类型">
       <template v-slot:prepend>
-        <q-icon name="phone" />
+        <q-icon name="event" />
+      </template>
+    </q-select>
+
+    <q-input readonly v-model="teach.startTime" prefix="开始日期:">
+      <template v-slot:prepend>
+        <q-icon name="event" class="cursor-pointer">
+          <q-popup-proxy transition-show="scale" transition-hide="scale">
+            <q-date v-model="teach.startTime" mask="YYYY-MM-DD HH:mm" />
+          </q-popup-proxy>
+        </q-icon>
+      </template>
+
+      <template v-slot:append>
+        <q-icon name="access_time" class="cursor-pointer">
+          <q-popup-proxy transition-show="scale" transition-hide="scale">
+            <q-time v-model="teach.startTime" mask="YYYY-MM-DD HH:mm" format24h />
+          </q-popup-proxy>
+        </q-icon>
       </template>
     </q-input>
 
-    <template>
-        <q-input readonly v-model="teach.endTime" prefix="日期:">
-          <template v-slot:prepend>
-            <q-icon name="event" class="cursor-pointer">
-              <q-popup-proxy transition-show="scale" transition-hide="scale">
-                <q-date v-model="teach.endTime" mask="YYYY-MM-DD HH:mm" />
-              </q-popup-proxy>
-            </q-icon>
-          </template>
+    <q-input readonly v-model="teach.endTime" prefix="结束日期:">
+      <template v-slot:prepend>
+        <q-icon name="event" class="cursor-pointer">
+          <q-popup-proxy transition-show="scale" transition-hide="scale">
+            <q-date v-model="teach.endTime" mask="YYYY-MM-DD HH:mm" />
+          </q-popup-proxy>
+        </q-icon>
+      </template>
 
-          <template v-slot:append>
-            <q-icon name="access_time" class="cursor-pointer">
-              <q-popup-proxy transition-show="scale" transition-hide="scale">
-                <q-time v-model="teach.endTime" mask="YYYY-MM-DD HH:mm" format24h />
-              </q-popup-proxy>
-            </q-icon>
-          </template>
-        </q-input>
-    </template>
+      <template v-slot:append>
+        <q-icon name="access_time" class="cursor-pointer">
+          <q-popup-proxy transition-show="scale" transition-hide="scale">
+            <q-time v-model="teach.endTime" mask="YYYY-MM-DD HH:mm" format24h />
+          </q-popup-proxy>
+        </q-icon>
+      </template>
+    </q-input>
 
     <q-input v-model="teach.place" type="text" prefix="辅导地点:">
       <template v-slot:prepend>
@@ -67,12 +83,13 @@ export default {
   data () {
     return {
       options: ['辅导', '讲座'],
+      type: '',
       teach: {
         id: 0,
         name: '',
         type: '1', // 0：辅导  1：讲座
         price: '500',
-        startTime: '2019-01-30 15:52',
+        startTime: '2019-11-30 15:52',
         endTime: '2019-12-30 15:52',
         place: 'A栋',
         phone: 14787461136,
@@ -84,15 +101,26 @@ export default {
   },
   methods: {
     onClickSubmit () {
-      const params = JSON.parse(JSON.stringify(this.teach))
-      params.startTime = date.formatDate(params.startTime, 'X')
-      params.endTime = date.formatDate(params.endTime, 'X')
-
-      this.$axios.post('/tutoring/save', params).then((res) => {
-        res.data.page.pageInfo.list.forEach(item => {
-          item.otherPic = 'http://47.106.222.50:8083' + this.splitMth(item.otherPic)
-          this.items.push(item)
-        })
+      for (let i = 0; i < this.options.length; i++) {
+        if (this.options[i] === this.type) {
+          this.teach.type = i
+          break
+        }
+      }
+      this.$axios.post('/tutoring/save', this.teach).then((res) => {
+        if (res.data.code === 100) {
+          this.$q.notify(res.data.msgs.msg)
+        }
+        else if (res.data.code === 200) {
+          let msg = ''
+          Object.keys(res.data.page.errors).forEach(key => {
+            msg = msg + key + res.data.page.errors[key]
+          })
+          this.$q.notify(msg)
+        }
+        else {
+          this.$q.notify('未知错误')
+        }
       })
     },
     // 初始化图书类型下拉框
@@ -101,6 +129,11 @@ export default {
       // this.options = value.map((item) => {
       //   return item.text
       // })
+    },
+    initData () {
+      // const params = JSON.parse(JSON.stringify(this.teach))
+      // params.startTime = date.formatDate(params.startTime, 'X')
+      // params.endTime = date.formatDate(params.endTime, 'X')
     }
   },
   components: {

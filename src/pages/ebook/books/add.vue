518c1d4355a0e6d22b15073a2f775c6bcf0e4220
gpj <1549361150@qq.com>
2019-12-06 21:38:00 +0800

    modify book add page

--- src/pages/ebook/books/add.vue
+++ src/pages/ebook/books/add.vue
@@ -1,66 +1,73 @@
 <template>
   <div class="q-pa-md" style="width: 100%">
-    <div class="row">
-      <div class="col-9 text-center" >
-        <span class="text-h5">编辑图书信息</span>
+    <form @submit.prevent.stop="onSubmit" class="q-gutter-md">
+      <div class="row">
+        <div class="col-9 text-center" >
+          <span class="text-h5">编辑图书信息</span>
+        </div>
+        <div class="col-3">
+          <q-btn rounded color="blue" label="发布" @click="onSubmit" />
+        </div>
       </div>
-      <div class="col-3">
-        <q-btn rounded color="blue" @click="onClickSubmit">发布</q-btn>
-      </div>
-    </div>
-    <br>
+      <br>
 
-    <pic-upload :urls="JSON.stringify(urls)" @filesArr="getPicFiles"/>
+      <pic-upload :urls="JSON.stringify(urls)" @filesArr="getPicFiles"/>
 
-    <q-input :rules="[val => val && val.length > 0 || '图书名称不能为空']"
-             lazy-rules ref="book.bookName" type="text" prefix="名称:"
-             value="" v-model="book.bookName">
-      <template v-slot:prepend>
-        <q-icon name="book" />
-      </template>
-    </q-input>
+      <q-input :rules="[val => val && val.length > 0 || '图书名称不能为空']"
+               ref="bookName" type="text" prefix="名称:"
+               value="" v-model="book.bookName">
+        <template v-slot:prepend>
+          <q-icon name="book" />
+        </template>
+      </q-input>
 
-    <q-input :rules="[val => val && val.length > 0 || '作者不能为空']"
-             lazy-rules ref="book.author" value="" v-model="book.author"
-             type="text" prefix="作者:">
-      <template v-slot:prepend>
-        <q-icon name="people" />
-      </template>
-    </q-input>
+      <q-input :rules="[val => val && val.length > 0 || '作者不能为空']"
+               ref="author" value="" v-model="book.author"
+               type="text" prefix="作者:">
+        <template v-slot:prepend>
+          <q-icon name="people" />
+        </template>
+      </q-input>
 
-    <q-select v-model="book.bookType" :options="getBookTypeNameArr" prefix="分类:">
-      <template v-slot:prepend>
-        <q-icon name="event" />
-      </template>
-    </q-select>
+      <q-input :rules="[val => val && val.length > 0 || '出版社不能为空']"
+               v-model="book.bookPub" ref="bookPub"
+               type="text" prefix="出版社:">
+        <template v-slot:prepend>
+          <q-icon name="mail" />
+        </template>
+      </q-input>
 
-    <q-input :rules="[val => val && val.length > 0 || '出版社不能为空']"
-             lazy-rules ref="book.bookPub" value="" v-model="book.bookPub"
-             type="text" prefix="出版社:">
-      <template v-slot:prepend>
-        <q-icon name="mail" />
-      </template>
-    </q-input>
+      <q-select :rules="[val => val && val.length > 0 || '图书类型不能为空']"
+                v-model="book.bookType" ref="bookType"
+                :options="getBookTypeNameArr" prefix="分类:"
+      >
+        <template v-slot:prepend>
+          <q-icon name="event" />
+        </template>
+      </q-select>
 
-    <q-input :rules="[val => val && val.length > 0 || '出版日期不能为空']"
-             lazy-rules ref="book.pubDate" readonly value=""
-             v-model="book.pubDate" prefix="出版日期:">
-      <template v-slot:prepend>
-        <q-icon name="send" />
-      </template>
-      <q-popup-proxy ref="qDateProxy" transition-show="scale" transition-hide="scale">
-        <date-time :date-string="book.pubDate" :is-time=false @input="setBookPubDate"/>
-      </q-popup-proxy>
-    </q-input>
+      <q-input :rules="[val => val && val.length > 0 || '出版日期不能为空']"
+               v-model="book.pubDate" ref="pubDate"
+               readonly prefix="出版日期:">
+        <template v-slot:prepend>
+          <q-icon name="send" />
+        </template>
+        <q-popup-proxy ref="qDateProxy" transition-show="scale" transition-hide="scale">
+          <date-time :date-string="book.pubDate" :is-time=false @input="setBookPubDate"/>
+        </q-popup-proxy>
+      </q-input>
 
-    <q-input value="" v-model="book.bookPrice"
-             type="number" prefix="出售价格:" suffix="￥">
-      <template v-slot:prepend>
-        <q-icon name="money" />
-      </template>
-    </q-input>
-    <br>
-    <q-editor v-model="book.des" value=""/>
+      <q-input :rules="[ val => val > 0 && val < 1000 || '出售价格非法']"
+               v-model="book.bookPrice" ref="bookPrice"
+               type="number" prefix="出售价格:" suffix="￥"
+      >
+        <template v-slot:prepend>
+          <q-icon name="money" />
+        </template>
+      </q-input>
+      <br>
+      <q-editor v-model="book.des" value=""/>
+    </form>
   </div>
 </template>
 
@@ -105,37 +112,63 @@ export default {
     getPicFiles (files) {
       this.book.files = files
     },
-    onClickSubmit () {
-      // 校验
-      this.$v.book.$touch()
-
-      if (this.$v.book.$error) {
-        return
-      }
-
+    async onSubmit () {
       let url = '/book/save'
       if (this.updateFlag) {
         url = '/book/update'
       }
+      // 判断图片是否为空
+      if (this.book.files.length < 1) {
+        this.$q.notify('至少添加一张图片')
+        return
+      }
+      this.$refs.bookName.validate()
+      this.$refs.author.validate()
+      this.$refs.bookPub.validate()
+      this.$refs.bookType.validate()
+      this.$refs.pubDate.validate()
+      this.$refs.bookPrice.validate()
+      // 校验
+      if (this.$refs.bookName.hasError ||
+        this.$refs.author.hasError ||
+        this.$refs.bookPub.hasError ||
+        this.$refs.bookType.hasError ||
+        this.$refs.pubDate.hasError ||
+        this.$refs.bookPrice.hasError) {
+        return
+      }
+
       const bookMsg = JSON.parse(JSON.stringify(this.book))
       bookMsg.files = this.book.files
       bookMsg.bookType = this.getBookTypeNumberByName(bookMsg.bookType)
-      this.$axios.post(url, bookMsg).then((res) => {
-        if (res.data.code === 100) {
-          this.$q.notify(res.data.msgs.msg)
-        }
-        else {
-          this.$q.notify('Fail')
-        }
-        // 跳转回原页面
-        this.$router.go(-1)
-      })
+      try {
+        this.$q.loading.show({
+          message: '上传中...'
+        })
+        await this.$axios.post(url, bookMsg).then((res) => {
+          if (res.data.code === 100) {
+            this.$q.notify(res.data.msgs.msg)
+          }
+          else {
+            this.$q.notify('Fail')
+          }
+          // 跳转回原页面
+          this.$router.go(-1)
+        })
+      }
+      catch (e) {
+        console.log(e)
+      }
+      finally {
+        this.$q.loading.hide()
+      }
     },
     getBookMsg () {
       const bookMsg = JSON.parse(this.getPageMsg)
       Object.keys(this.book).forEach(key => {
         this.book[key] = bookMsg[key]
       })
+      console.log(this.book.bookPrice)
       this.urls = bookMsg.url
     }
   },

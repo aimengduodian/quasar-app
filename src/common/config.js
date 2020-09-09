6ff78ced3bef7bc08bfd2fc74818dd7e8c9e9cc3
gpj <1549361150@qq.com>
2020-09-09 11:55:20 +0800

    rollback

--- src/common/config.js
+++ src/common/config.js
@@ -2,8 +2,8 @@
 const version = '0.0.1'
 
 // 服务器地址
-// const baseUrl = 'http://47.106.222.50:8080/ebooks/'
-const baseUrl = 'http://77142166b44a.ngrok.io/'
+const baseUrl = 'http://47.106.222.50:8080/ebooks/'
+// const baseUrl = 'http://77142166b44a.ngrok.io/'
 const picUrl = 'http://47.106.222.50:8083'
 const timeout = 600000
 

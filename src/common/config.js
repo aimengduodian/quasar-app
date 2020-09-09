cd99dded8b4414a8de8a6fedccfd7b5087ff89e4
gpj <1549361150@qq.com>
2020-09-09 11:52:41 +0800

    add node sass

--- src/common/config.js
+++ src/common/config.js
@@ -2,16 +2,16 @@
 const version = '0.0.1'
 
 // 服务器地址
-const baseUrl = 'http://47.106.222.50:8080/ebooks/'
-// const baseUrl = 'https://96ce0aa2.ngrok.io'
+// const baseUrl = 'http://47.106.222.50:8080/ebooks/'
+const baseUrl = 'http://77142166b44a.ngrok.io/'
 const picUrl = 'http://47.106.222.50:8083'
-const timeout = 60000
+const timeout = 600000
 
 // 每次请求列表加载的条数
 const pageSize = 10
 //
-const reRequestBaseInterval = 60000
-const reRequestAddInterval = 10000
+const reRequestBaseInterval = 600000
+const reRequestAddInterval = 100000
 
 export default {
   version,
